2616dfb6d0e53228310a073d85133e5916e87067
Gar|k <garik.djan@gmail.com>
2019-06-22 02:31:11 +0300

    refactor: func do

--- gitstats.go
+++ gitstats.go
@@ -32,9 +32,16 @@ type author struct {
 	ID    int    `json: "id"`
 }
 
+type stats struct {
+	Additions int `json: "additions"`
+	Deletions int `json: "deletions"`
+	Total     int `json: "total"`
+}
+
 type client struct {
 	baseURL *url.URL
 	http    *fasthttp.HostClient
+	values  map[int]map[string]int
 }
 
 const defaultConfigFile = "config.json"
@@ -51,23 +58,49 @@ func loadConfiguration(file string) (config, error) {
 	return config, nil
 }
 
+func (c *client) do(url string, v interface{}) error {
+	statusCode, body, err := c.http.Get(nil, url)
+	if err != nil {
+		return err
+	}
+	if statusCode != fasthttp.StatusOK {
+		return fmt.Errorf("Unexpected status code: %d. Expecting %d", statusCode, fasthttp.StatusOK)
+	}
+
+	return json.Unmarshal(body, v)
+}
+
 func (c *client) commits(repository *repository) ([]commit, error) {
+	var commits []commit
+
 	rel := &url.URL{
 		Path: "repos/" + repository.Owner + "/" + repository.Name + "/commits",
 	}
 	u := c.baseURL.ResolveReference(rel)
 
-	statusCode, body, err := c.http.Get(nil, u.String())
-	if err != nil {
-		return nil, err
-	}
-	if statusCode != fasthttp.StatusOK {
-		return nil, fmt.Errorf("Unexpected status code: %d. Expecting %d", statusCode, fasthttp.StatusOK)
-	}
-
-	var commits []commit
-	err = json.Unmarshal(body, &commits)
+	err := c.do(u.String(), &commits)
 	return commits, err
+}
+
+func (c *client) stats(repository *repository, ref string) (stats, error) {
+	var stats stats
+
+	rel := &url.URL{
+		Path: "repos/" + repository.Owner + "/" + repository.Name + "/commits/" + ref,
+	}
+	u := c.baseURL.ResolveReference(rel)
+
+	err := c.do(u.String(), &stats)
+	return stats, err
+}
+
+func (c *client) add(id int, key string, value int) {
+	mm, ok := c.values[id]
+	if !ok {
+		mm = make(map[string]int)
+		c.values[id] = mm
+	}
+	mm[key] += value
 }
 
 func main() {
@@ -92,5 +125,10 @@ func main() {
 	if err2 != nil {
 		panic(err2)
 	}
-	fmt.Printf("%+v\n", commits)
+	fmt.Printf("%+v\n", commits[0])
+	stats, err3 := client.stats(&config.Repositories[0], commits[1].Sha)
+	if err3 != nil {
+		panic(err3)
+	}
+	fmt.Printf("%+v\n", stats)
 }
